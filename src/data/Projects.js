512af9a995352019c7996207c331bbc6d8b41d10
Frank Martinez <frankie.martinez@gmail.com>
2020-03-16 13:01:43 -0700

    🗃 Added project info

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,10 +1,22 @@
 export default [
     {
         id: 0,
-        name: "Revitalize",
-        description: "",
-        link: "https://revitalize.community",
+        name: "Lakeside Herbal Solutions",
+        description: "Closed source project for retail dispensary serving more than 1000 users a month. Fully responsive across almost any device, built with no framework, ~1 second load time with 3.2mb payload, custom bootstrap, HTML/CSS, minimal JS and PHP, integrated with store's POS system for real-time menu data.",
+        link: "https://lakesideherbal.com",
         github: "",
+        iconUrl:
+            "https://res.cloudinary.com/letanque/image/upload/v1583875610/greencross_ds9eez.png",
+        imageUrl:
+            "https://res.cloudinary.com/letanque/image/upload/v1583875434/lakeside-logo-dark_1080x1080_k4shid.png",
+        date: "December, 2018"
+    },
+    {
+        id: 1,
+        name: "Revitalize",
+        description: 'Crowdfunding platform that creates apprenticeship opportunities. "Labs" project; 3 month intense multilateral team project using agile development methodologies. Aka, super fun! Build with CRA, graphQL/prisma/apollo, mapbox, stripe, passport.js and much more.',
+        link: "https://revitalize.community",
+        github: "https://github.com/Revitalized-Playground",
         iconUrl:
             "https://res.cloudinary.com/letanque/image/upload/v1583875267/Favicon_ypavsw.png",
         imageUrl:
@@ -12,15 +24,27 @@ export default [
         date: "November, 2019"
     },
     {
-        id: 1,
-        name: "Lakeside",
-        description: "",
-        link: "https://lakesideherbal.com",
-        github: "",
+        id: 2,
+        name: "Lambda Notes",
+        description: "React/Redux build sprint project; full CRUD note taking application. Build sprints are usually group projects, but due to circumstances outside of my control, I completed the project solo. Built with CRA, Redux, Postgres/REST API, and custom responsive mosaic layout.",
+        link: "https://lambda-notes-build-week.now.sh/",
+        github: "https://github.com/LeTanque/lambda-notes-build-week",
         iconUrl:
-            "https://res.cloudinary.com/letanque/image/upload/v1583875610/greencross_ds9eez.png",
+            "https://res.cloudinary.com/letanque/image/upload/v1584387527/icon-webnoted256plusShadow_qqpwkc.png",
         imageUrl:
-            "https://res.cloudinary.com/letanque/image/upload/v1583875434/lakeside-logo-dark_1080x1080_k4shid.png",
-        date: "February, 2019"
+            "https://res.cloudinary.com/letanque/image/upload/v1584387527/Screen_Shot_2020-03-16_at_12.15.31_PM_tbgewm.png",
+        date: "March, 2019"
+    }
+    {
+        id: 3,
+        name: "MTG Magical",
+        description: "MTG card collection tools. Magic the Gathering is a strategy card game enjoyed by millions worldwide. This is a sideproject created to provide tools custom deck builders want, like card search, collection and discovery. Built with CRA, Postgres/REST API, multiple card image/data APIs, and more.",
+        link: "https://mtg-magical.netlify.com/",
+        github: "https://github.com/LeTanque/MTG-card-collection",
+        iconUrl:
+            "https://res.cloudinary.com/letanque/image/upload/v1584387625/magical-screenshot-03_ldefwg.png",
+        imageUrl:
+            "https://res.cloudinary.com/letanque/image/upload/v1584387626/magical-screenshot-02_ot49x9.png",
+        date: "July, 2019"
     }
 ];
