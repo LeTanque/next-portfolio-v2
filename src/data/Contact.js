32a25ccfcbac6a77099a1450a045ee3b17938b36
Frank Martinez <frankie.martinez@gmail.com>
2020-03-20 10:01:50 -0700

    🗃 Updated email address
    Having some problems with mail server, playing it safe by just updating to my gmail

--- src/data/Contact.js
+++ src/data/Contact.js
@@ -1,7 +1,7 @@
 export default [
     {
         phone: "4088005266",
-        email: "get@frank3.me",
+        email: "frank.ie.martinez@gmail.com",
         github: "https://github.com/letanque",
         linkedin: "https://www.linkedin.com/in/frankm3/",
         devto: "https://dev.to/letanque"
